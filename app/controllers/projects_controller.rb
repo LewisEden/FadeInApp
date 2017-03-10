158333191fce4fd42aa644318200123c831bcea7
Lewis <lewis@fadeinapp.com>
2017-03-10 17:40:12 +0000

    allow projects to be created

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,15 @@ class ProjectsController < ApplicationController
 	def new
 		@project = Project.new
 	end
+
+	def create
+		@project = Project.create(project_params)
+		redirect_to posts_path
+	end
+
+	private
+
+	def post_params
+		params.require(:project).permit(:image, :genre, :duration, :title, :synopsis, :director, :writer, :actor, :editor, :composer, :cinematographer, :makeup)
+	end
 end
