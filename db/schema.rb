72247fa87d54918b6de9329e5123959bb763e7b3
Atticus <atticusstormpirrie@gmail.com>
2017-04-22 13:42:09 +0000

    devise fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422132049) do
+ActiveRecord::Schema.define(version: 20170422134042) do
 
   create_table "challenges", force: :cascade do |t|
     t.string   "genre"
@@ -132,6 +132,7 @@ ActiveRecord::Schema.define(version: 20170422132049) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
